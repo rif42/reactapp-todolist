bd4b29f79b95dac5a4b3fe0db174711bfbf1b0f9
rifky <ee.guy.42@gmail.com>
2021-12-06 01:03:31 +0700

    deleted unimportant files

--- src/App.js
+++ src/App.js
@@ -15,50 +15,86 @@ import Register from './pages/Register'
 
 function App() 
 {
-  var Aranio = [
-    "Aranio" ,
-    "822,75", 
-    "6" , 
-    "5,7875", 
-    "56,71825", 
-    "6,5375", 
-    "7,315",
-    "0,33265",
-    "0,389075",
-    ];
-  localStorage.setItem("AR",JSON.stringify(Aranio));
+  // const Aranio = [
+  //   {fecalcoli:822.75},
+  //   {TSS:6},
+  //   {BOD:5.7875},
+  //   {COD:56.71825},
+  //   {DO:6.5375},
+  //   {pH:7.315},
+  //   {N:0.33625},
+  //   {PO:0.38905}
+  // ]
+
+  // const test = Aranio.map(yoink);
+  // function yoink(item){
+  //   return [Object.entries(item)];
+  // }
+
+  //hide add task form
+  const [showAddTask, setShowAddTask] = useState(false)
+
+  //delete task
+
+  const [tasklist, setTasks] = useState(
+    [
+        {
+          id:1,
+          objective : "Last hit lane creeps",  
+          minute : 5,
+          lasthit : 120,
+          reminder : true
+        },
+        {
+          id:2,
+          objective : "Farm jungle",  
+          minute : 10,
+          lasthit : 80,
+          reminder : false
+        },
+        {
+          id:3,
+          objective : "Get key items",  
+          minute : 20,
+          lasthit : 150,
+          reminder : true
+        }
+    ]
+  )
   
-  var KarangIntan = [
-    "Karang Intan" ,
-    "4475,25",
-    "6,75" , 
-    "13,5775", 
-    "46,29775", 
-    "7,23", 
-    "7,34",
-    "0,36825",
-    "0,06825",
-    ];
-  localStorage.setItem("KI",JSON.stringify(KarangIntan));
-  
-  var SungaiKembang = [
-    "Sungai Kembang" ,
-    "5425",
-    "6,75" , 
-    "1,3075", 
-    "20,892", 
-    "9,4375", 
-    "7,195",
-    "0,3295",
-    "0,056325",
-    ];
+  //add task
+  function addTask(task){
+    const id = Math.floor(Math.random()*1000)+1
+    const newTask = {id, ...task}
+    setTasks([...tasklist, newTask])
+  }
+
+  //deleting task
+  function deleteTask(index) {
+    setTasks(tasklist.filter((task) => task.id !== index))
+  }
+
+  //toggle reminder
+  function toggleReminder(index){
+    setTasks(tasklist.map((task)=>task.id === index 
+    ? {...task, reminder: !task.reminder } 
+    : task))
+  };
+
+  let tasklistjson = JSON.stringify(tasklist);
+  localStorage.setItem('data',tasklistjson);
   
   return (
     <div className="container">
-      <p> Sungai Aranio </p>
-      <p> fecal coli : {Aranio[1]}</p>
+      <Header onAdd={() => setShowAddTask(!showAddTask)} 
+      showAdd={showAddTask}
+      title="Dota Task Tracker"/>
+      {showAddTask && <AddTask onAdd={addTask}/>}
+      {tasklist.length > 0 
+      ? (<Tasks tasklist={tasklist} onDelete={deleteTask} onToggle={toggleReminder}/>) 
+      : ( "Error 404 tasks not found")}
     </div>
   )
 }
-
+    
 export default App;
